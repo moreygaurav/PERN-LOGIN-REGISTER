8ef93de1e284f65857b8bdfc0d864e74e0064fd1
GauravMorey <gaurav1234pmorey@gmail.com>
2026-06-04 20:35:39 +0530

    Project restructure and backend configuration

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+import {Pool} from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+pool.on ('connect', () => {
+    console.log('Connected to the database'); 
+});
+
+pool.on ('error', (err) => {
+    console.error('Database error:', err);
+    
+});
+
+export default Pool;
